61b7c795531e46c826864a293c9eed850b4b04ae
Matteo Vaccari <vaccari@pobox.com>
2024-04-09 12:04:29 +0200

    java: add a TestCase builder

--- java/src/test/java/org/example/IndexTemplateTest.java
+++ java/src/test/java/org/example/IndexTemplateTest.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
 
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -15,44 +16,83 @@ import static org.assertj.core.api.Assertions.assertThat;
 class IndexTemplateTest {
     record TestCase(String name,
                     TodoList model,
+                    String url,
                     String selector,
                     List<String> matches) {
         @Override
         public String toString() {
             return name;
         }
+
+        public static final class Builder {
+            String name;
+            TodoList model;
+            String url;
+            String selector;
+            List<String> matches;
+
+            public Builder name(String name) {
+                this.name = name;
+                return this;
+            }
+
+            public Builder model(TodoList model) {
+                this.model = model;
+                return this;
+            }
+
+            public Builder url(String url) {
+                this.url = url;
+                return this;
+            }
+
+            public Builder selector(String selector) {
+                this.selector = selector;
+                return this;
+            }
+
+            public Builder matches(String ... matches) {
+                this.matches = Arrays.asList(matches);
+                return this;
+            }
+
+            public TestCase build() {
+                return new TestCase(name, model, url, selector, matches);
+            }
+        }
     }
 
-    public static TestCase[] indexTestCases() {
-        return new TestCase[]{
-                new TestCase(
-                        "all todo items are shown",
-                        new TodoList()
+    public static TestCase.Builder[] indexTestCases() {
+        return new TestCase.Builder[]{
+                new TestCase.Builder()
+                        .name("all todo items are shown")
+                        .model(new TodoList()
                                 .add("Foo")
-                                .add("Bar"),
-                        "ul.todo-list li",
-                        List.of("Foo", "Bar")),
-                new TestCase(
-                        "completed items get the 'completed' class",
-                        new TodoList()
+                                .add("Bar"))
+                        .selector("ul.todo-list li")
+                        .matches("Foo", "Bar"),
+                new TestCase.Builder()
+                        .name("completed items get the 'completed' class")
+                        .model(new TodoList()
                                 .add("Foo")
-                                .addCompleted("Bar"),
-                        "ul.todo-list li.completed",
-                        List.of("Bar")),
-                new TestCase(
-                        "items left",
-                        new TodoList()
+                                .addCompleted("Bar"))
+                        .selector("ul.todo-list li.completed")
+                        .matches("Bar"),
+                new TestCase.Builder()
+                        .name("items left")
+                        .model(new TodoList()
                                 .add("One")
                                 .add("Two")
-                                .addCompleted("Three"),
-                        "span.todo-count",
-                        List.of("2 items left")),
+                                .addCompleted("Three"))
+                        .selector("span.todo-count")
+                        .matches("2 items left"),
         };
     }
 
     @ParameterizedTest
     @MethodSource("indexTestCases")
-    void testIndexTemplate(TestCase test) {
+    void testIndexTemplate(TestCase.Builder testBulder) {
+        var test = testBulder.build();
         var html = renderTemplate("/index.tmpl", test.model);
 
         var document = parseHtml(html);
